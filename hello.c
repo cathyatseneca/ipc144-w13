3bb0b77003fd53cafff8588bc36428804247b73a
Catherine Leung <catherine.leung@senecacollege.ca>
2013-01-11 14:22:14 -0500

    updated hello.c with comments

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*this is a comment
+  cathy's ipc program number 1*/
+//this is a single line comment
+//#include is a preprocessor statement
+//you need this if you want to use input or output statements
+#include <stdio.h>
+
+//below is our main function
+//it returns(gives back) an integer
+//it takes as arguments void or in otherwords nothing
+int main(void){
+    //the line below calls a function called printf
+    //printf outputs to the console
+    printf("hello world!\n");
+}
