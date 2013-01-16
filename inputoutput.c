124dfa1322f49a28ab4a59a4d00114a5f44e57c1
Catherine Leung <catherine.leung@senecacollege.ca>
2013-01-16 15:15:01 -0500

    inputoutput.c

---
+++ inputoutput.c
@@ -0,0 +1,20 @@
+/*
+  This program will ask the user to enter in 2 numbers and 
+  then it will calculate its sum, difference, production, and
+  quotient
+
+  Author: Catherine Leung
+*/
+#include <stdio.h>
+
+int main(void){
+  int number1;//this is a variable, that will hold the first number the user enters
+  int number2;//this is the variable that will hold the 2nd number the user enters 
+  printf("Please enter a number: ");  //ask for number
+  scanf("%d",&number1);  //reads a number in from standard input
+                         //%d is a format code that matches the data type
+                         //of number1
+                         //& is the address of operator
+                         //we are not passing the variable number1 into the 
+                         //function but rather its location in memory
+}
