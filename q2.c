6b33796d5a1d095c754b510b43043a8b6bd1dded
Catherine Leung <catherine.leung@senecacollege.ca>
2013-02-22 09:42:00 -0500

    fixed q2.c

--- q2.c
+++ q2.c
@@ -2,9 +2,10 @@
 
 int main(void){
    int value;
+   int i;
    printf("please enter a number between 1 and 12: ");
    scanf("%d",&value);
-   for(int i=0;i<12;i++){
+   for(i=0;i<12;i++){
      printf("%d * %d = %d\n",value, i+1, value*(i+1));
    }
 }
