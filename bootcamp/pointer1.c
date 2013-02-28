54e5e2b4728f92fb491dde0226cad5eb8693130c
Catherine Leung <cathyatseneca@gmail.com>
2013-02-28 11:34:08 -0500

    bootcamp

---
+++ bootcamp/pointer1.c
@@ -0,0 +1,8 @@
+/*
+  this function will pass back the first and last digit of value through
+  the pointers first and second
+*/
+void getFirstLastDigit(int value, int* first, int* second);
+int main(void){
+  int firstdigit, lastdigit;
+}
