3463632584a06fac98c081b22b69440bd7e5d078
Catherine Leung <cathyatseneca@gmail.com>
2013-04-05 14:23:00 -0400

    test 2 answers

---
+++ mags.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void){
+  int year;
+  
+  FILE* fp;
+  printf("Please enter a year: ");
+  scanf("%d",&year);
+
+  fp=fopen("magazine.dat","r");
+  if(fp){
+    int i;
+    char magName[41];
+    char title[61];
+    int magYear;
+    while(fscanf(fp,"%[^;];%[^;];%d\n", magName, title, &magYear)==3){
+      if(year == magYear){
+        printf("%s - %s\n", magName, title);
+      }
+    }
+  }
+  else{
+    printf("cannot open file magazine.dat\n");
+  }
+}
+    
